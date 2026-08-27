4c3e0e2521d7bcefb74e77aaac29f7ca8010d87f
ivangzn <gonzalezmartiniv@gmail.com>
2026-08-27 20:56:48 -0300

    i18n: add jp

--- modules/user/basic.nix
+++ modules/user/basic.nix
@@ -18,6 +18,46 @@
     enable = true;
   };
 
+  # Language
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.waylandFrontend = true; # Crucial for Niri / Wayland
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
+  };
+
+  # Set Super+Space as the toggle hotkey
+  xdg.configFile."fcitx5/config".text = ''
+    [Hotkey]
+    TriggerKeys=
+    0=Super+space
+
+    [Hotkey/TriggerKeys]
+    0=Super+space
+  '';
+
+  # Set your active layout profile
+  xdg.configFile."fcitx5/profile".text = ''
+    [Groups/0]
+    Name=Default
+    Default Layout=latam
+    DefaultIM=mozc
+
+    [Groups/0/Items/0]
+    Name=keyboard-latam
+    Layout=
+
+    [Groups/0/Items/1]
+    Name=mozc
+    Layout=
+
+    [GroupOrder]
+    0=Default
+  '';
+
   # Dark theme
   gtk = {
     enable = true;
